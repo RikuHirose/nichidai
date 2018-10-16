8a8c8db40d1fbf57f1d30aa826c81421ded5b844
RikuHirose <rikuparkour9@gmail.com>
2018-10-16 22:08:15 +0700

    share_sns_lesson

--- resources/views/shared/user/sidebar.blade.php
+++ resources/views/shared/user/sidebar.blade.php
@@ -1,14 +1,31 @@
-
-<div class="sidebar">
-  <div class="sidebar_title">
-    <h4></h4>
-  </div>
-
-  @each('components.user.sidebar.card', $models, 'model')
-
-  <!-- <div class="card" style="height: 200px;">
-    <div class="card-body">
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+<div class="col-md-3 col-xs-12">
+  @isset($sidebar_content['recommend_lessons'])
+  <h3>オススメ授業</h3>
+    <div class="col-md-12 col-xs-12">
+        @include('shared.user.sidebarContent', ['models' => $sidebar_content['recommend_lessons']])
     </div>
-  </div> -->
+  @endisset
+
+  @isset($sidebar_content['popular_lessons'])
+  <h3>人気授業</h3>
+    <div class="col-md-12 col-xs-12">
+        @include('shared.user.sidebarContent', ['models' => $sidebar_content['popular_lessons']])
+    </div>
+  @endisset
+
+  @isset($sidebar_content['other_articles'])
+  <h3>オススメ記事</h3>
+    <div class="col-md-12 col-xs-12">
+        @include('components.user.sidebar.otherArticle', ['models' => $sidebar_content['other_articles']])
+    </div>
+  @endisset
+
+  @isset($authUser)
+    @isset($sidebar_content['history_lessons'])
+    <h3>あなたが閲覧した授業</h3>
+      <div class="col-md-12 col-xs-12">
+          @include('shared.user.sidebarContent', ['models' => $sidebar_content['history_lessons']])
+      </div>
+    @endisset
+  @endisset
 </div>
---
+++ resources/views/shared/user/sidebarContent.blade.php
@@ -0,0 +1,14 @@
+
+<div class="sidebar">
+  <div class="sidebar_title">
+    <h4></h4>
+  </div>
+
+  @each('components.user.sidebar.card', $models, 'model')
+
+  <!-- <div class="card" style="height: 200px;">
+    <div class="card-body">
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    </div>
+  </div> -->
+</div>
