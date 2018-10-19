daeeb05f0366d5eb849780ba8f1be9539535f543
RikuHirose <rikuparkour9@gmail.com>
2018-10-19 12:51:22 +0700

    fix-styles-search-form

--- resources/views/components/user/lessons/card.blade.php
+++ resources/views/components/user/lessons/card.blade.php
@@ -1,30 +1,32 @@
 <div class="col-md-4 col-sm-6 col-xs-12 " style="height: 500px;">
-  <div class="card" >
+  <div class="card">
       <div class="card-body">
         @include('components.user.lessons.favorite', ['model' => $model])
         <h5 class="card-title"><a class="" href="{{ route('lessons.show', [$model->id]) }}">{{ $model->lesson_title }}<small>({{ $model->year }})</small></a></h5>
 
-        <h6 class="card-subtitle mb-2 text-muted">{{ $model->present()->sub_title }}</h6>
+        <h6 class="card-subtitle text-muted">{{ $model->present()->sub_title }}</h6>
+        <div>
+          <table class="table table-condensed">
+            <tbody>
+              <tr class="table-header">
+                <th>開講</th>
+                <th>曜日･時限</th>
+                <th>単位</th>
+                <th>担当</th>
+              </tr>
+              <tr>
+                <td>{{ $model->lesson_term }}</td>
+                <td>{{ $model->lesson_date }}{{ $model->lesson_hour }}</td>
+                <td>{{ $model->lesson_credit }}<br></td>
+                <td>{{ $model->lesson_professor }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
 
-        <table class="table table-condensed">
-          <tbody>
-            <tr class="table-header">
-              <th>講義開講時期</th>
-              <th>曜日･時限</th>
-              <th>単位</th>
-              <th>担当者名</th>
-            </tr>
-            <tr>
-              <td>{{ $model->lesson_term }}</td>
-              <td>{{ $model->lesson_date }}</td>
-              <td>{{ $model->lesson_credit }}<br></td>
-              <td>{{ $model->lesson_professor }}</td>
-            </tr>
-          </tbody>
-        </table>
 
         <div class="table-responsive">
-          <table class="table table-condensed">
+         <!--  <table class="table table-condensed">
             <tbody>
               <tr>
                 @foreach($model->present()->card_evaluate as $k => $v)
@@ -33,6 +35,17 @@
                   <td class="hidden-sm hidden-md hidden-lg"></td>
                 @endforeach
               </tr>
+            </tbody>
+          </table> -->
+          <table class="table table-condensed evaluate-table">
+            <tbody>
+              @foreach($model->present()->card_evaluate as $k => $v)
+              <tr>
+                  <td class="mw60 success text-success">{{ $k }}</td>
+                  <td class="mw60">{{ $v }}%</td>
+                  <td class="hidden-sm hidden-md hidden-lg"></td>
+              </tr>
+              @endforeach
             </tbody>
           </table>
         </div>
--- resources/views/pages/user/lessons/index.blade.php
+++ resources/views/pages/user/lessons/index.blade.php
@@ -28,7 +28,7 @@
 <!-- lessons -->
 <div class="col-xs-12">
   @if(isset($title))
-    <h2>{{ $title }} @if($models->currentPage() !== 1) {{ $models->currentPage() }}ページ目 @endif</h2>
+    <h3>{{ $title }} @if($models->currentPage() !== 1) {{ $models->currentPage() }}ページ目 @endif</h3>
   @endif
   <div class="row">
     @each('components.user.lessons.card', $models, 'model')
